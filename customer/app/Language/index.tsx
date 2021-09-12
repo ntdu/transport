8eaa35c7a244f47c8a2ac5331d749cb5273068c1
unknown <ntdu198@gmail.com>
2021-09-12 22:01:14 +0700

    Update sign in

--- customer/app/Language/index.tsx
+++ customer/app/Language/index.tsx
@@ -1,23 +1,23 @@
-// import { useEffect } from 'react'
-// import { setI18nConfig, translate } from './localize'
-// import * as RNLocalize from 'react-native-localize'
+import { useEffect } from 'react'
+import { setI18nConfig, translate } from './localize'
+import * as RNLocalize from 'react-native-localize'
 
-// const Language = () => {
-//   setI18nConfig()
+const Language = () => {
+  setI18nConfig()
 
-//   useEffect(() => {
-//     RNLocalize.addEventListener('change', handleLocalizationChange)
-//     return () =>
-//       RNLocalize.removeEventListener('change', handleLocalizationChange)
-//   }, [])
+  useEffect(() => {
+    RNLocalize.addEventListener('change', handleLocalizationChange)
+    return () =>
+      RNLocalize.removeEventListener('change', handleLocalizationChange)
+  }, [])
 
-//   const handleLocalizationChange = () => {
-//     setI18nConfig()
-//   }
+  const handleLocalizationChange = () => {
+    setI18nConfig()
+  }
 
-//   return null
-// }
+  return null
+}
 
-// export { translate }
+export { translate }
 
-// export default Language
+export default Language
